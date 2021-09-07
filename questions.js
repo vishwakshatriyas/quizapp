51699539831982e4f9aac4f82b06a2a0815b834d
vishwakshatriyas <90241370+vishwakshatriyas@users.noreply.github.com>
2021-09-07 12:31:45 +0530

    Upload files

---
+++ questions.js
@@ -0,0 +1,227 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does html stands for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The correct sequence of HTML tags for starting a webpage is ?",
+    answer: "HTML,Head,Title,Body",
+    options: [
+      "Head,Title,HTML,Body",
+      "HTML,Body,Title,Head",
+      "HTML,Head,Title,Body",
+      "HTML,Title,Head,Body"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is used to create web pages?",
+    answer: "HTML",
+    options: [
+      "HTML",
+      "C",
+      "JVM",
+      "DTD"
+    ]
+  },
+    {
+    numb: 4,
+    question: "HTML document is saved using ____ extenstion?",
+    answer: ".html",
+    options: [
+      ".htl",
+      ".html",
+      ".hml",
+      ".htnl"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many heading tags are supported by HTML?",
+    answer: "6",
+    options: [
+      "3",
+      "4",
+      "5",
+      "6"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The tags in HTML are ?",
+    answer: "not case sensitive",
+    options: [
+      "case-sensitive",
+      "in upper case",
+       "not case sensitive",
+      "in lower case"
+    ]
+ },
+ {
+   numb: 7,
+   question: "Which of the following tag is used for inserting the largest heading in HTML ?",
+   answer: "< h1 >",
+   options: [
+     "< h3 >",
+     "< h1 >",
+     "< h5 >",
+     "< h6 >"
+   ]
+ },
+ {
+   numb: 8,
+   question: "HTML tags are enclosed in-",
+   answer: "< and >",
+   options: [
+     "# and #",
+     "{ and }",
+     "! and !",
+     "< and >"
+   ]
+ },
+
+ {
+   numb: 9,
+   question: "CSS stands for ?",
+   answer: "Cascading style sheets",
+   options: [
+     "Cascade style sheets",
+     "Color and style sheets",
+     "Cascading style sheets",
+     "none of the above"
+   ]
+  },
+  {
+    numb: 10,
+    question: "The property in CSS used to change the text color of an element is ?",
+    answer: "color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "back-color"
+    ]
+  },
+  {
+    numb: 11,
+    question: "The CSS property used to specify the transparency of an element is ?",
+    answer: "opacity",
+    options: [
+      "hover",
+      "opacity",
+      "clearfix",
+      "overlay"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which oof the following CSS function allows us to perform calculations ?",
+    answer: "calc() function",
+    options: [
+      "calc() function",
+      "calculator() function",
+      "calculate() function",
+      "cal() function"
+    ]
+    
+  },
+  {
+    numb: 13,
+    question: "Which type of JavaScript language is ?",
+    answer: "object-based",
+    options: [
+      "object-oriented",
+      "object-based",
+      "assembly-language",
+      "high-level"
+    ]
+  },
+  {
+    numb: 14,
+    question: "The function and var are known as ?",
+    answer: "declaration statements",
+    options: [
+      "keywords",
+      "data types",
+      "declaration statements",
+      "prototypes"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which one of the following keywords is used for defining the function in the javascript ?",
+    answer: "function",
+    options: [
+      "void",
+      "init",
+      "main",
+      "function"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which of the following type of a variable is volatile ?",
+    answer: "mutable variable",
+    options: [
+      "mutable variable",
+      "dynamic variable",
+      "volatile variable",
+      "immutable variable"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which of the following HTML tag is the special formatting tag ?",
+    answer: "< pre >",
+    options: [
+      "< p >",
+      "< b >",
+      "< pre >",
+      "< br >"
+    ]
+  },
+  {
+    numb: 18,
+    question: "What does the < br > tag add to your webpage ?",
+    answer: "line break",
+    options: [
+      "long break",
+      "paragraph break",
+      "line break",
+      "short break"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Which tag will you add to specify a font for your whole page ?",
+    answer: "< defaultfont >",
+    options: [
+      "< defaultfont >",
+      "< targetfont >",
+      "< basefont >",
+      "< font >"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Gif and jpg are the two main type of ?",
+    answer: "images",
+    options: [
+      "animated effects",
+      "videos",
+      "images",
+      "none of the above"
+    ]
+  },
+
+  
+];
